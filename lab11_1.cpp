dbc9bedb2e054b6481ddadd73f9e26278ad7189c
Ayayay1231 <champ25752@gmail.com>
2025-12-30 04:03:31 +0700

    lab11-1 successed

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,39 @@
 // cout << "Press Enter 3 times to reveal your future.";
 // cout << "You will get A in this 261102.";
+
+
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+
+using namespace std ;
+
+int main(){
+    string grade ;
+    cout << "Press Enter 3 times to reveal your future." ;
+    cin.get();
+    cin.get();
+    cin.get();
+    srand(time(0));
+    int i = rand()%9;
+    if(i == 1){
+        grade = "A" ;
+    }else if(i == 2){
+        grade = "B+" ;
+    }else if(i == 3){
+        grade = "B" ;
+    }else if(i == 4){
+        grade = "C+" ;
+    }else if(i == 5){
+        grade = "C" ;
+    }else if(i == 6){
+        grade = "D+" ;
+    }else if(i == 7){
+        grade = "D" ;
+    }else if(i == 8){
+        grade = "F" ;
+    }else{
+        grade = "W" ;
+    }
+    cout << "You will get " << grade <<" in this 261102." ;
+}
